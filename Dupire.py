c9d3ef13cd451dfd8739aa7edae769d5b33fdef4
Wissg <wissal.ghoudi@gmail.com>
2023-01-15 13:10:49 +0100

    ++

--- Dupire.py
+++ Dupire.py
@@ -151,7 +151,7 @@ def LevenbergMarquardGatheral(S0, r, Tmax, Kmax, M, N, epsilon, lamb, Kp, Vp, Be
         m = m + d[1]
     print("k = ", k)
     print("Hesienne = ", Hesienne)
-    return a, b
+    return a, m
 
 
 Kmax = 20
@@ -167,102 +167,123 @@ epsilon = 0.00001
 lamb = 0.001
 K = np.linspace(0, Kmax, N + 2)
 T = np.linspace(0, Tmax, M + 2)
-# V = Dupire_Price(Kmax, S0, r, Tmax, N, M, Beta1, Beta2, h)
+V = Dupire_Price(Kmax, S0, r, Tmax, N, M, Beta1, Beta2, h)
 
-# plt.plot(K, V[0, :])
-# plt.xlabel("K")
-# plt.ylabel("V")
-# plt.title("Price Dupire T = 0")
-# plt.show()
-#
-# plt.plot(K, V[floor((M+1)/2), :])
-# plt.xlabel("K")
-# plt.ylabel("V")
-# plt.title("Price Dupire T/2")
-# plt.show()
+plt.plot(K, V[0, :])
+plt.xlabel("K")
+plt.ylabel("V")
+plt.title("Price Dupire T = 0")
+plt.savefig('Graph\Price_Dupire_T_=_0.png')
+plt.show()
 
-# plt.plot(K, V[M+1, :])
-# plt.xlabel("K")
-# plt.ylabel("V")
-# plt.title("Price Dupire T")
-# plt.show()
+plt.plot(K, V[floor((M+1)/2), :])
+plt.xlabel("K")
+plt.ylabel("V")
+plt.title("Price Dupire T/2")
+plt.savefig('Graph\Price_Dupire_T_2.png')
+plt.show()
 
-# V = Dupire_Price(Kmax, S0, r, Tmax, N, M, Beta1, Beta2, h, 0.3)
-#
-# plt.plot(K, V[0, :])
-# plt.xlabel("K")
-# plt.ylabel("V")
-# plt.title("Price Dupire T = 0 sigmal= 0,3")
-# plt.show()
-#
-# plt.plot(K, V[floor((M+1)/2), :])
-# plt.xlabel("K")
-# plt.ylabel("V")
-# plt.title("Price Dupire T/2 sigmal= 0,3")
-# plt.show()
-#
-# plt.plot(K, V[M+1, :])
-# plt.xlabel("K")
-# plt.ylabel("V")
-# plt.title("Price Dupire T sigmal= 0,3")
-# plt.show()
-#
-#
-# fig = plt.figure()
-# k, t = np.meshgrid(K, T)
-# ax = fig.add_subplot(projection='3d')
-# ax.plot_surface(k, t, V)
-# ax.set_title("Price Dupire")
-# ax.set_xlabel("K")
-# ax.set_ylabel("T")
-# ax.set_zlabel("V")
-# # plt.legend()
-# plt.show()
+plt.plot(K, V[M+1, :])
+plt.xlabel("K")
+plt.ylabel("V")
+plt.title("Price Dupire T")
+plt.savefig('Graph\Price_Dupire_T_max.png')
+plt.show()
 
-# Vega = Vega_Dupire(Kmax, S0, r, Tmax, N, M, Beta1, Beta2, h)
-#
-# plt.plot(K, Vega[M+1, :])
-# plt.xlabel("K")
-# plt.ylabel("Vega")
-# plt.title("Vega Dupire Tmax with sigma loc = 1/K")
-# plt.show()
-#
-#
-# fig = plt.figure()
-# k, t = np.meshgrid(K, T)
-# ax = fig.add_subplot(projection='3d')
-# ax.plot_surface(k, t, Vega)
-# ax.set_title("Vega Dupire with sigma loc = 1/K")
-# ax.set_xlabel("K")
-# ax.set_ylabel("T")
-# ax.set_zlabel("Vega")
-# plt.show()
-#
-# Vega = Vega_Dupire(Kmax, S0, r, Tmax, N, M, Beta1, Beta2, h, 0.3)
-#
-# plt.plot(K, Vega[M+1, :])
-# plt.xlabel("K")
-# plt.ylabel("Vega")
-# plt.title("Vega Dupire Tmax with sigma loc = 0.3")
-# plt.show()
-#
-#
-# fig = plt.figure()
-# k, t = np.meshgrid(K, T)
-# ax = fig.add_subplot(projection='3d')
-# ax.plot_surface(k, t, Vega)
-# ax.set_title("Vega Dupire with sigma loc = 0.3")
-# ax.set_xlabel("K")
-# ax.set_ylabel("T")
-# ax.set_zlabel("Vega")
-# plt.show()
+fig = plt.figure()
+k, t = np.meshgrid(K, T)
+ax = fig.add_subplot(projection='3d')
+ax.plot_surface(k, t, V)
+ax.set_title("Price Dupire")
+ax.set_xlabel("K")
+ax.set_ylabel("T")
+ax.set_zlabel("V")
+# plt.legend()
+plt.savefig('Graph\Price_Dupire_3d.png')
+plt.show()
 
-# Kp = [7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14]
-# Vp = [3.3634, 2.9092, 2.4703, 2.0536, 1.6666, 1.3167, 1.0100, 0.7504, 0.5389, 0.3733, 0.2491, 0.1599, 0.0986, 0.0584,
-#       0.0332]
-#
-# Beta1, Beta2 = LevenbergMarquard(S0, r, Tmax, Kmax, M, N, epsilon, lamb, Kp, Vp, Beta1, Beta2)
-# print("Beta1 = ", Beta1, " Beta2 =", Beta2)
+V = Dupire_Price(Kmax, S0, r, Tmax, N, M, Beta1, Beta2, h, 0.3)
+
+plt.plot(K, V[0, :])
+plt.xlabel("K")
+plt.ylabel("V")
+plt.title("Price Dupire T = 0 sigmal= 0,3")
+plt.savefig('Graph\Price_Dupire_T_0_sigmal_0-3.png')
+plt.show()
+
+plt.plot(K, V[floor((M+1)/2), :])
+plt.xlabel("K")
+plt.ylabel("V")
+plt.title("Price Dupire T/2 sigmal= 0,3")
+plt.savefig('Graph\Price_Dupire_T_2_sigmal_0-3.png')
+plt.show()
+
+plt.plot(K, V[M+1, :])
+plt.xlabel("K")
+plt.ylabel("V")
+plt.title("Price Dupire T sigmal= 0,3")
+plt.savefig('Graph\Price_Dupire_T_max_sigmal_0-3.png')
+plt.show()
+
+fig = plt.figure()
+k, t = np.meshgrid(K, T)
+ax = fig.add_subplot(projection='3d')
+ax.plot_surface(k, t, V)
+ax.set_title("Price Dupire")
+ax.set_xlabel("K")
+ax.set_ylabel("T")
+ax.set_zlabel("V")
+# plt.legend()
+plt.savefig('Graph\Price_Dupire_3d_sigmal_0-3.png')
+plt.show()
+
+Vega = Vega_Dupire(Kmax, S0, r, Tmax, N, M, Beta1, Beta2, h)
+
+plt.plot(K, Vega[M+1, :])
+plt.xlabel("K")
+plt.ylabel("Vega")
+plt.title("Vega Dupire Tmax with sigma loc = 1/K")
+plt.savefig('Graph\Price_Dupire_sigmal_0-1-K.png')
+plt.show()
+
+fig = plt.figure()
+k, t = np.meshgrid(K, T)
+ax = fig.add_subplot(projection='3d')
+ax.plot_surface(k, t, Vega)
+ax.set_title("Vega Dupire with sigma loc = 1/K")
+ax.set_xlabel("K")
+ax.set_ylabel("T")
+ax.set_zlabel("Vega")
+plt.savefig('Graph\Price_Dupire_3d_sigmal_0-1-K.png')
+plt.show()
+
+Vega = Vega_Dupire(Kmax, S0, r, Tmax, N, M, Beta1, Beta2, h, 0.3)
+
+plt.plot(K, Vega[M+1, :])
+plt.xlabel("K")
+plt.ylabel("Vega")
+plt.title("Vega Dupire Tmax with sigma loc = 0.3")
+plt.savefig('Graph\Vega_Dupire_sigmal_0-3.png')
+plt.show()
+
+
+fig = plt.figure()
+k, t = np.meshgrid(K, T)
+ax = fig.add_subplot(projection='3d')
+ax.plot_surface(k, t, Vega)
+ax.set_title("Vega Dupire with sigma loc = 0.3")
+ax.set_xlabel("K")
+ax.set_ylabel("T")
+ax.set_zlabel("Vega")
+plt.savefig('Graph\Vega_Dupire_3d_sigmal_0-3.png')
+plt.show()
+
+Kp = [7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14]
+Vp = [3.3634, 2.9092, 2.4703, 2.0536, 1.6666, 1.3167, 1.0100, 0.7504, 0.5389, 0.3733, 0.2491, 0.1599, 0.0986, 0.0584,
+      0.0332]
+
+Beta1, Beta2 = LevenbergMarquard(S0, r, Tmax, Kmax, M, N, epsilon, lamb, Kp, Vp, Beta1, Beta2)
+print("Beta1 = ", Beta1, " Beta2 =", Beta2)
 
 Vm = [5.2705, 4.3783, 3.5510, 2.8138, 2.1833, 1.6651, 1.2541, 0.9374, 0.6983, 0.5195, 0.3851, 0.2817, 0.1987, 0.1277]
 Kp = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
@@ -271,81 +292,88 @@ m = 5
 rho = 0.1
 b = 0.05
 
-# V = Dupire_Price(Kmax, S0, r, Tmax, N, M, Beta1, Beta2, h,'GATHERAL',a,b,rho,m)
-#
-# plt.plot(K, V[M+1, :])
-# plt.xlabel("K")
-# plt.ylabel("V")
-# plt.title("Price Dupire Gatheral")
-# plt.show()
-#
-# fig = plt.figure()
-# k, t = np.meshgrid(K, T)
-# ax = fig.add_subplot(projection='3d')
-# ax.plot_surface(k, t, V)
-# ax.set_title("Price Dupire Gatheral")
-# ax.set_xlabel("K")
-# ax.set_ylabel("T")
-# ax.set_zlabel("V")
-# # plt.legend()
-# plt.show()
+V = Dupire_Price(Kmax, S0, r, Tmax, N, M, Beta1, Beta2, h,'GATHERAL',a,b,rho,m)
 
-# Vega = Vega_Dupire(Kmax, S0, r, Tmax, N, M, Beta1, Beta2, h,'GATHERAL',a,b,rho,m)
-#
-# plt.plot(K, Vega[M + 1, :])
-# plt.xlabel("K")
-# plt.ylabel("Vega")
-# plt.title("Vega Dupire Gatheral")
-# plt.show()
-#
-# fig = plt.figure()
-# k, t = np.meshgrid(K, T)
-# ax = fig.add_subplot(projection='3d')
-# ax.plot_surface(k, t, Vega)
-# ax.set_title("Vega Dupire Gatheral")
-# ax.set_xlabel("K")
-# ax.set_ylabel("T")
-# ax.set_zlabel("Vega")
-# plt.show()
+plt.plot(K, V[M+1, :])
+plt.xlabel("K")
+plt.ylabel("V")
+plt.title("Price Dupire Gatheral")
+plt.savefig('Graph\Price_Dupire_Gatheral.png')
+plt.show()
+
+fig = plt.figure()
+k, t = np.meshgrid(K, T)
+ax = fig.add_subplot(projection='3d')
+ax.plot_surface(k, t, V)
+ax.set_title("Price Dupire Gatheral")
+ax.set_xlabel("K")
+ax.set_ylabel("T")
+ax.set_zlabel("V")
+# plt.legend()
+plt.savefig('Graph\Price_Dupire_Gatheral_3d.png')
+plt.show()
+
+Vega = Vega_Dupire(Kmax, S0, r, Tmax, N, M, Beta1, Beta2, h,'GATHERAL',a,b,rho,m)
+
+plt.plot(K, Vega[M + 1, :])
+plt.xlabel("K")
+plt.ylabel("Vega")
+plt.title("Vega Dupire Gatheral")
+plt.savefig('Graph\Vega_Dupire_Gatheral.png')
+plt.show()
+
+fig = plt.figure()
+k, t = np.meshgrid(K, T)
+ax = fig.add_subplot(projection='3d')
+ax.plot_surface(k, t, Vega)
+ax.set_title("Vega Dupire Gatheral")
+ax.set_xlabel("K")
+ax.set_ylabel("T")
+ax.set_zlabel("Vega")
+plt.savefig('Graph\Vega_Dupire_Gatheral_3d.png')
+plt.show()
 
 Beta1 = 1
 Beta2 = 1
-# a, m = LevenbergMarquardGatheral(S0, r, Tmax, Kmax, M, N, epsilon, lamb, Kp, Vm, Beta1, Beta2, a, b, rho, m,'GATHERAL')
+a, m = LevenbergMarquardGatheral(S0, r, Tmax, Kmax, M, N, epsilon, lamb, Kp, Vm, Beta1, Beta2, a, b, rho, m,'GATHERAL')
 print("a = ", a, " m =",m)
-a =  10.202707764970771
-m = 0.05
-m =12.00874008
+print(b)
 Beta1 = a
 Beta2 = m
-y = np.zeros(len(K))
-for i in range(len(K)):
-    y[i] = sigma_locale_gatheral(K, i, Beta1, Beta2, h, a, b, rho, m)
+y = np.zeros(len(Kp))
+for i in range(len(Kp)):
+    y[i] = sigma_locale_gatheral(Kp, i, Beta1, Beta2, h, a, b, rho, m)
 fig, ax = plt.subplots()
-ax.plot(K, y)
-
+ax.plot(Kp, y)
+plt.xlabel("K")
+plt.ylabel("sigma local")
+plt.title("Sigma local Gaheral")
+plt.savefig('Graph\Sigma_local_Gaheral.png')
 plt.show()
 
-# V = Dupire_Price(Kmax, S0, r, Tmax, N, M, Beta1, Beta2, h,'GATHERAL',a,b,rho,m)
-#
-# fig = plt.figure()
-# k, t = np.meshgrid(K, T)
-# ax = fig.add_subplot(projection='3d')
-# ax.plot_surface(k, t, V)
-# ax.set_title("Price Dupire Gatheral Calibrée")
-# ax.set_xlabel("K")
-# ax.set_ylabel("T")
-# ax.set_zlabel("V")
-# # plt.legend()
-# plt.show()
+V = Dupire_Price(Kmax, S0, r, Tmax, N, M, Beta1, Beta2, h,'GATHERAL',a,b,rho,m)
+
+fig = plt.figure()
+k, t = np.meshgrid(K, T)
+ax = fig.add_subplot(projection='3d')
+ax.plot_surface(k, t, V)
+ax.set_title("Price Dupire Gatheral Calibrée")
+ax.set_xlabel("K")
+ax.set_ylabel("T")
+ax.set_zlabel("V")
+# plt.legend()
+plt.savefig('Graph\Price_Dupire_Gatheral_Calibrée.png')
+plt.show()
 
 Vega = Vega_Dupire(Kmax, S0, r, Tmax, N, M, Beta1, Beta2, h, 'GATHERAL', a, b, rho, m)
 
-# fig = plt.figure()
-# k, t = np.meshgrid(K, T)
-# ax = fig.add_subplot(projection='3d')
-# ax.plot_surface(k, t, Vega)
-# ax.set_title("Vega Dupire Gatheral Calibrée")
-# ax.set_xlabel("K")
-# ax.set_ylabel("T")
-# ax.set_zlabel("Vega")
-# plt.show()
+fig = plt.figure()
+k, t = np.meshgrid(K, T)
+ax = fig.add_subplot(projection='3d')
+ax.plot_surface(k, t, Vega)
+ax.set_title("Vega Dupire Gatheral Calibrée")
+ax.set_xlabel("K")
+ax.set_ylabel("T")
+ax.set_zlabel("Vega")
+plt.savefig('Graph\Vega_Dupire_Gatheral_Calibrée.png')
+plt.show()
